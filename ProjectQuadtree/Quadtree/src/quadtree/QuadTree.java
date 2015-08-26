4bcad9c67ff0f43a3459adda3aac44fb58b90ef4
SammyGuevara <sammygm6@gmail.com>
2015-08-26 08:57:12 -0600

    arbol y nodos

--- ProjectQuadtree/Quadtree/src/quadtree/QuadTree.java
+++ ProjectQuadtree/Quadtree/src/quadtree/QuadTree.java
@@ -1,4 +1,26 @@
 package quadtree;
 public class QuadTree {
+    
+    private nodo raiz;
 
+    public QuadTree() {
+    }
+    
+    public QuadTree(nodo raiz) {
+        this.raiz = raiz;
+    }
+
+    public nodo getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(nodo raiz) {
+        this.raiz = raiz;
+    }
+
+    @Override
+    public String toString() {
+        return "QuadTree{" + "raiz=" + raiz + '}';
+    }
+    
 }
