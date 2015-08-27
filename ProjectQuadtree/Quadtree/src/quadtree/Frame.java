57e5080bb8c2088b327820321b91a4ad9505953f
OscarCarranza <oscar.carranza@unitec.edu>
2015-08-26 21:53:06 -0600

    Casi listo

--- ProjectQuadtree/Quadtree/src/quadtree/Frame.java
+++ ProjectQuadtree/Quadtree/src/quadtree/Frame.java
@@ -426,9 +426,6 @@ public class Frame extends javax.swing.JFrame {
                         break;
                     }
                 }
-                /*if (lastPixReached) {
-                    break;
-                }*/
             }
             if (lastPixReached && depth <= (int)sp_depth.getValue() && image.getWidth() > 4 && image.getHeight() > 4) {
                 
@@ -460,7 +457,7 @@ public class Frame extends javax.swing.JFrame {
         BufferedImage resized = new BufferedImage(w,h,BufferedImage.TYPE_INT_RGB);
         
         Graphics2D g2 = resized.createGraphics();
-        g2.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
         g2.drawImage(img,0,0,w,h,null);
         
         return resized;
